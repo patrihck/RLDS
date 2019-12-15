3af3fa5b800544f974c7a7dc17001df712433bab
Alan Biegun <alanbiegun13@gmail.com>
2019-12-15 10:33:29 +0100

    Group table added

---
+++ src/RldsAppCoreDb/Tables/Group.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Group]
+(
+	[GroupId] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NULL, 
+    [Info] NVARCHAR(128) NULL, 
+    [Ordinal] NVARCHAR(128) NULL, 
+    [ts] NVARCHAR(10) NULL
+)
