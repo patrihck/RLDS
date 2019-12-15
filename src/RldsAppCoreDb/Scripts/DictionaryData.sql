f451c3a3ac0a030cf6a3f61f18ff3813421be2f4
jan.snopek <jan.snopek@4836-B-2018-2>
2019-12-15 11:29:52 +0100

    Role table / State table Dictionary

---
+++ src/RldsAppCoreDb/Scripts/DictionaryData.sql
@@ -0,0 +1,22 @@
+﻿-- Role table --
+
+if not exists(select * from dbo.Role where RoleName = 'User')
+insert into dbo.Role(RoleName) values(N'User');
+
+if not exists(select * from dbo.Role where RoleName = 'SuperUser')
+insert into dbo.Role(RoleName) values(N'SuperUser');
+
+if not exists(select * from dbo.Role where RoleName = 'Admin')
+insert into dbo.Role(RoleName) values(N'Admin');
+
+
+-- State Table --
+
+
+if not exists(select * from dbo.TransactionState where Name = 'Not Started') insert into dbo.TransactionState(Name, Ordinal) values(N'Not Started', 0);
+
+if not exists(select * from dbo.TransactionState where Name = 'In Progress') insert into dbo.TransactionState(Name, Ordinal) values(N'In Progress', 0);
+
+if not exists(select * from dbo.TransactionState where Name = 'Completed') insert into dbo.TransactionState(Name, Ordinal) values(N'Completed', 0);
+
+
