e727c99aa320ce1629ed2cbd8d244ca99e0329da
Gryfny <jan.snopek.86@gmail.com>
2020-01-03 18:52:11 +0000

    Jsnopek

---
+++ src/RldsApp.Data/Entities/TransactionState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RldsApp.Data.Entities
+{
+    public class TransactionState : IVersionedEntity
+    {
+        public virtual long TransactionStateId { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual byte[] Version { get; set; }
+
+        public virtual int Ordinal { get; set; }
+    }
+}
--- src/RldsAppCoreDb/Tables/Group.sql
+++ src/RldsAppCoreDb/Tables/Group.sql
@@ -3,6 +3,6 @@
     [GroupId] INT NOT NULL PRIMARY KEY, 
     [Name] NVARCHAR(50) NULL, 
     [Info] NVARCHAR(128) NULL, 
-    [Ordinal] NVARCHAR(128) NULL, 
-    [ts] NVARCHAR(10) NULL
+    [Ordinal] INT NULL, 
+    [ts] ROWVERSION NULL
 )
