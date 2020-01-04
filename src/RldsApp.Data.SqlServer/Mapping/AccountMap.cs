f46b094d71da271e4abc6463d6f7a73a31b8f2ac
Alan Biegun <alanbiegun13@gmail.com>
2020-01-04 19:13:13 +0300

    Added account map

---
+++ src/RldsApp.Data.SqlServer/Mapping/AccountMap.cs
@@ -0,0 +1,18 @@
+﻿using FluentNHibernate.Mapping;
+using RldsApp.Data.Entities;
+
+namespace RldsApp.Data.SqlServer.Mapping
+{
+	public class AcccountMap : VersionedClassMap<Account>
+	{
+		public AcccountMap()
+		{
+			Id(x => x.AccountId, "AccountId");
+			Map(x => x.StartAmount, "StartAmount").Not.Nullable(); ;
+			References(x => x.Currency, "CurrencyId");
+			References(x => x.Group, "GroupId");
+			References(x => x.AccountType, "AccountTypeId");
+		}
+	}
+
+}
