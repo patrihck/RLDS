36d6ac70d0b178c51cbecad8605190e10cdaa052
Damian Kostrzewski <damian.kostrzewski@hotmail.com>
2020-01-04 18:09:40 +0000

    #20  Add category mapping entity, Refactor entities according to relations

--- src/RldsApp.Data/Entities/AccountType.cs
+++ src/RldsApp.Data/Entities/AccountType.cs
@@ -6,15 +6,13 @@ namespace RldsApp.Data.Entities
 {
     public class AccountType : IVersionedEntity
 	{
-		private readonly IList<User> _users = new List<User>();
-
 		public virtual long AccountTypeId { get; set; }
+
+		public virtual User User { get; set; }
 
 		public virtual string Name { get; set; }
 
 		public virtual string Description { get; set; }
-
-		public virtual IList<User> User => _users;
 
 		public virtual byte[] Version { get; set; }
 	}
--- src/RldsApp.Data/Entities/Role.cs
+++ src/RldsApp.Data/Entities/Role.cs
@@ -1,10 +1,16 @@
-﻿namespace RldsApp.Data.Entities
+﻿using System.Collections.Generic;
+
+namespace RldsApp.Data.Entities
 {
 	public class Role : IVersionedEntity
 	{
+		private readonly IList<User> _users = new List<User>();
+
 		public virtual long RoleId { get; set; }
 
 		public virtual string RoleName { get; set; }
+
+		public virtual IList<User> Users => _users;
 
 		public virtual byte[] Version { get; set; }
 	}
