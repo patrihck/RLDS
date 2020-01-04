0c15fc8f6b0f6fd8e9f0df075b8aa2631960a38c
Alan Biegun <alanbiegun13@gmail.com>
2020-01-04 19:21:28 +0300

    Deleted user field from account

--- src/RldsApp.Data/Entities/Account.cs
+++ src/RldsApp.Data/Entities/Account.cs
@@ -8,8 +8,6 @@ namespace RldsApp.Data.Entities
 
 		public int AccountId { get; set; }
 
-        public User User { get; set; }
-
         public Currency Currency { get; set; }
 
         public Group Group { get; set; }
