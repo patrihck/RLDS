a20c0515f287c7230d78e52c592872c902f78fad
jan.snopek <jan.snopek@4836-B-2018-2>
2020-02-22 11:41:59 +0100

    Status and Group automapper #34 #35

---
+++ src/RldsApp.Web.Api/AutoMappingConfiguration/StatusProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using RldsApp.Web.Api.Models;
+
+
+namespace RldsApp.Web.Api.AutoMappingConfiguration
+{
+    public class StatusProfile : Profile
+    {
+        public StatusProfile()
+        {
+            CreateMap<Status, Data.Entities.Status>()
+                .ForMember(dest => dest.Version, opt => opt.Ignore());
+
+            CreateMap<Data.Entities.Status, Status>()
+                .ForMember(dest => dest.Links, opt => opt.Ignore());
+
+        }
+    }
+}
