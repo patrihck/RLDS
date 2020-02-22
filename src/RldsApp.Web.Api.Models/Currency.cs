57f8f50607624d7d9be1048e7fa6e7082929f78f
Patryk Kwaśniewski <pkwasniewski3@gmail.com>
2020-02-22 10:37:44 +0100

    currency auto mapping

---
+++ src/RldsApp.Web.Api.Models/Currency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RldsApp.Web.Api.Models
+{
+    public class Currency
+    {
+        [Key]
+        public long? CurrencyId { get; set; }
+
+        public string CurrencyName { get; set; }
+
+        public string CurrencySymbol { get; set; }
+    }
+}
---
+++ src/RldsApp.Web.Api/AutoMappingConfiguration/CurrencyProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using RldsApp.Web.Api.Models;
+
+namespace RldsApp.Web.Api.AutoMappingConfiguration
+{
+	public class CurrencyProfile : Profile
+	{
+		public CurrencyProfile()
+		{
+			CreateMap<Currency, Data.Entities.Currency>()
+				.ForMember(dest => dest.Version, opt => opt.Ignore());
+		}
+	}
+}
