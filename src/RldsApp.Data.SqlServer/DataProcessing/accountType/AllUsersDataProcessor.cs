f4ceaa23d86a5cf5827b9d16e88508097abfc0a0
Patryk Kwaśniewski <pkwasniewski3@gmail.com>
2020-03-21 16:44:56 +0100

    business logic for getting account types

---
+++ src/RldsApp.Data.SqlServer/DataProcessing/accountType/AllUsersDataProcessor.cs
@@ -0,0 +1,23 @@
+﻿using NHibernate;
+using RldsApp.Data.DataProcessing.accountType;
+using RldsApp.Data.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RldsApp.Data.SqlServer.DataProcessing.accountTypes
+{
+	public class AllAccountTypesDataProcessor : IAllAccountTypesDataProcessor
+	{
+		private readonly ISession _session;
+
+		public AllAccountTypesDataProcessor(ISession session)
+		{
+			_session = session;
+		}
+
+		public List<AccountType> GetAccountTypes()
+		{
+			return _session.Query<AccountType>().ToList();
+		}
+	}
+}
