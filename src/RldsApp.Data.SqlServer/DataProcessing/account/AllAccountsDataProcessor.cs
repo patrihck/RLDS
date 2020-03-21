bfda832ad3abf34530f4e34cf36453cc0617dff3
Patryk Kwaśniewski <pkwasniewski3@gmail.com>
2020-03-21 20:50:47 +0100

    implemented business logic for getting an account

---
+++ src/RldsApp.Data.SqlServer/DataProcessing/account/AllAccountsDataProcessor.cs
@@ -0,0 +1,23 @@
+﻿using NHibernate;
+using RldsApp.Data.DataProcessing.account;
+using RldsApp.Data.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RldsApp.Data.SqlServer.DataProcessing.account
+{
+	public class AllAccountsDataProcessor : IAllAccountsDataProcessor
+	{
+		private readonly ISession _session;
+
+		public AllAccountsDataProcessor(ISession session)
+		{
+			_session = session;
+		}
+
+		public List<Account> GetAccounts()
+		{
+			return _session.Query<Account>().ToList();
+		}
+	}
+}
---
+++ src/RldsApp.Data/DataProcessing/account/IDeleteAccountDataProcessor.cs
@@ -0,0 +1,7 @@
+﻿namespace RldsApp.Data.DataProcessing.account
+{
+	public interface IDeleteAccountDataProcessor
+	{
+		bool DeleteAccount(long accountId);
+	}
+}
