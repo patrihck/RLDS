66b1a9b5cc9a55ad9602286a6863dbb9e65fd3c0
damian.kostrzewski@hotmail.com <6f637765-8fc9-4544-a564-923fba541752>
2020-12-05 18:12:17 +0100

    Add missing files

---
+++ src/RldsApp.Data/DataProcessing/TransactionRuleDataProcessor/IAddTransactionRuleDataProcessor.cs
@@ -0,0 +1,9 @@
+﻿using RldsApp.Data.Entities;
+
+namespace RldsApp.Data.DataProcessing.TransactionRuleDataProcessor
+{
+    public interface IAddTransactionRuleDataProcessor
+    {
+        void AddTransactionRule(TransactionRule transactionRule);
+    }
+}
