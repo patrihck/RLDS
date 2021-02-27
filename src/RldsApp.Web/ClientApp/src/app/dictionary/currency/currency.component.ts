882797ebe658d3d79f06cfa3560c50d1ddad5a80
Alan Biegun <alanbiegun13@gmail.com>
2021-02-27 17:25:03 +0100

    Added currency and transaction category lists and delete option

--- src/RldsApp.Web/ClientApp/src/app/dictionary/currency/currency.component.ts
+++ src/RldsApp.Web/ClientApp/src/app/dictionary/currency/currency.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { defaultRequestErrorHandler } from '../../../infrastructure/interceptors/error-interceptor';
+import { PrimengTableColumn } from '../../../infrastructure/models/layout/primeng-table.model';
+import { Currency, CurrencyClient, PagedDataInquiryResponseOfCurrency } from '../../../infrastructure/services-api/rlds-api';
+import { LayoutService, MessageSeverity } from '../../../infrastructure/services/layout/layout.service';
 
 @Component({
   selector: 'app-currency',
@@ -6,10 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./currency.component.css']
 })
 export class CurrencyComponent implements OnInit {
+  currencies: PagedDataInquiryResponseOfCurrency;
+  columns: Array<PrimengTableColumn>;
 
-  constructor() { }
+  constructor(private readonly currencyClient: CurrencyClient, private readonly layoutService: LayoutService) { }
 
   ngOnInit(): void {
+    this.layoutService.setAppTitle("Konta");
+    this.createTable();
+    this.getData();
   }
 
+  getData() {
+    this.currencyClient.getCurrencies("1.0").subscribe(r => {
+      this.currencies = r;
+    }, err => defaultRequestErrorHandler(this.layoutService, err))
+  }
+  createTable() {
+    this.columns = new Array<PrimengTableColumn>();
+
+    this.columns.push(new PrimengTableColumn(o => {
+      o.field = 'id';
+      o.header = 'Id';
+    }));
+    this.columns.push(new PrimengTableColumn(o => {
+      o.field = 'name';
+      o.header = 'Nazwa';
+    }));
+    this.columns.push(new PrimengTableColumn(o => {
+      o.field = 'acronym';
+      o.header = 'Akromin';
+    }));
+    this.columns.push(new PrimengTableColumn(o => {
+      o.field = 'symbol';
+      o.header = 'Symbol';
+    }));
+    this.columns.push(new PrimengTableColumn(o => {
+      o.header = '';
+      o.field = 'options';
+    }));
+  }
+
+  remove(id: number) {
+    this.currencyClient.deleteCurrency(id, '1.0').subscribe(r => {
+      this.layoutService.showPopover(MessageSeverity.success, 'Usunięto walutę');
+    }, err => defaultRequestErrorHandler(this.layoutService, err));
+  }
 }
--- src/RldsApp.Web/ClientApp/src/app/dictionary/transaction-category/transaction-category.component.ts
+++ src/RldsApp.Web/ClientApp/src/app/dictionary/transaction-category/transaction-category.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { defaultRequestErrorHandler } from '../../../infrastructure/interceptors/error-interceptor';
+import { PrimengTableColumn } from '../../../infrastructure/models/layout/primeng-table.model';
+import { PagedDataInquiryResponseOfTransactionCategory, TransactionCategory, TransactionCategoryClient } from '../../../infrastructure/services-api/rlds-api';
+import { LayoutService, MessageSeverity } from '../../../infrastructure/services/layout/layout.service';
 
 @Component({
   selector: 'app-transaction-category',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionCategoryComponent implements OnInit {
 
-  constructor() { }
+  transactionCategories: PagedDataInquiryResponseOfTransactionCategory;
+  columns: Array<PrimengTableColumn>;
+
+  constructor(private readonly transactionCategoryClient: TransactionCategoryClient, private readonly layoutService: LayoutService) { }
 
   ngOnInit(): void {
+    this.layoutService.setAppTitle("Konta");
+    this.createTable();
+    this.getData();
   }
 
+  getData() {
+    this.transactionCategoryClient.getTransactionCategories("1.0").subscribe(r => {
+      this.transactionCategories = r;
+    }, err => defaultRequestErrorHandler(this.layoutService, err))
+  }
+  createTable() {
+    this.columns = new Array<PrimengTableColumn>();
+
+    this.columns.push(new PrimengTableColumn(o => {
+      o.field = 'id';
+      o.header = 'Id';
+    }));
+    this.columns.push(new PrimengTableColumn(o => {
+      o.field = 'name';
+      o.header = 'Nazwa';
+    }));
+    this.columns.push(new PrimengTableColumn(o => {
+      o.header = '';
+      o.field = 'options';
+    }));
+  }
+
+  remove(id: number) {
+    this.transactionCategoryClient.deleteTransactionCategory(id, '1.0').subscribe(r => {
+      this.layoutService.showPopover(MessageSeverity.success, 'Usunięto kategorię transakcji');
+    }, err => defaultRequestErrorHandler(this.layoutService, err));
+  }
 }
